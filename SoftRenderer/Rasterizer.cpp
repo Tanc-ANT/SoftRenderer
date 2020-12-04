c39a21cea2ee6b891a9aaa3843982483d4b2ae50
zhengyiyuan <914777731@qq.com>
2020-12-04 15:38:12 +0800

    Redundant code deletion

--- SoftRenderer/Rasterizer.cpp
+++ SoftRenderer/Rasterizer.cpp
@@ -11,24 +11,6 @@ Vertex mesh[8] = {
 	{Vector4(-1.0f, 1.0f,-1.0f,1.0f),Color(0.2f,1.0f,0.3f)},	//7
 };
 
-static Vector4 MatrixVectorMul(const Matrix4& m, const Vector4& v)
-{
-	return Vector4(	
-		m.m[0][0] * v.x	+ m.m[0][1] * v.y + m.m[0][2] * v.z + m.m[0][3] * v.w,
-		m.m[1][0] * v.x + m.m[1][1] * v.y + m.m[1][2] * v.z + m.m[1][3] * v.w,
-		m.m[2][0] * v.x + m.m[2][1] * v.y + m.m[2][2] * v.z + m.m[2][3] * v.w,
-		m.m[3][0] * v.x + m.m[3][1] * v.y + m.m[3][2] * v.z + m.m[3][3] * v.w);
-}
-
-static Vector4 MatrixVectorMul(const Vector4& v, const Matrix4& m)
-{
-	return Vector4(
-		v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + v.w * m.m[3][0],
-		v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + v.w * m.m[3][1],
-		v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + v.w * m.m[3][2],
-		v.x * m.m[0][3] + v.y * m.m[1][3] + v.z * m.m[2][3] + v.w * m.m[3][3]);
-}
-
 Rasterizer::Rasterizer()
 {
 	SetBoxNormal();
